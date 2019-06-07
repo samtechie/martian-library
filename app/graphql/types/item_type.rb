0dd9a8987ee39ea6d38a109a2cdd455bc0c1edb4
Wasswa Samuel <latimerscope@gmail.com>
2019-06-07 13:25:52 +0200

    Add apollo and mdify Item to include user

--- app/graphql/types/item_type.rb
+++ app/graphql/types/item_type.rb
@@ -3,5 +3,6 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :description, String, null: true
+    field :user, Types::UserType, null: false
   end
 end
