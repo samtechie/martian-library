c98d3df15d61f586949e34f6e8d0910220a49516
Wasswa Samuel <latimerscope@gmail.com>
2019-06-11 19:36:29 +0200

    Make first Graphql query

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -3,4 +3,9 @@ import React from "react";
 import { render } from "react-dom";
 import Provider from "../components/Provider";
 
-render(<Provider>👻</Provider>, document.querySelector("#root"));
+render(
+  <Provider>
+    <Library />
+  </Provider>,
+  document.querySelector("#root")
+);
